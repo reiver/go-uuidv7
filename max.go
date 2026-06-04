bdf074a47314f9b0a5c92343247d4fb1c35d7a4d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-06-04 14:46:51 -0700

    docs

--- max.go
+++ max.go
@@ -4,6 +4,11 @@ var max [16]byte = [16]byte{0xff,0xff,0xff,0xff,0xff,0xff,0x7f,0xff,0xbf,0xff,0x
 
 // Max returns the maximum value for a UUIDv7.
 // I.e., ffffffff-ffff-7fff-bfff-ffffffffffff.
+//
+//
+// Note that not all values between the minimum UUIDv7 and the maximum UUIDv7 are valid UUIDv7.
+// For example, 11111111-1111-1111-1111-111111111111 is between the minimum UUIDv7 and the maximum UUIDv7 but is NOT a valid UUIDv7.
+// You can use [Is] and [IsString] to determine if something is a valid UUIDv7.
 func Max() [16]byte {
 	return max
 }
--- min.go
+++ min.go
@@ -4,6 +4,10 @@ var min [16]byte = [16]byte{0x00,0x00,0x00,0x00,0x00,0x00,0x70,0x00,0x80,0x00,0x
 
 // Min returns the minimum value for a UUIDv7.
 // I.e., 00000000-0000-7000-8000-000000000000.
+//
+// Note that not all values between the minimum UUIDv7 and the maximum UUIDv7 are valid UUIDv7.
+// For example, 11111111-1111-1111-1111-111111111111 is between the minimum UUIDv7 and the maximum UUIDv7 but is NOT a valid UUIDv7.
+// You can use [Is] and [IsString] to determine if something is a valid UUIDv7.
 func Min() [16]byte {
 	return min
 }
