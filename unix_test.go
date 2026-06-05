0b2760b362a355edc938de34ccc36540f2ca8413
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-06-04 20:52:31 -0700

    tests

--- unix_test.go
+++ unix_test.go
@@ -17,7 +17,7 @@ func TestUnixTimeMilli(t *testing.T) {
 		},
 		{
 			UUIDv7:   uuidv7.Max(),
-			Expected: 281474976710655,
+			Expected: 0xFFFFFFFF_FFFF, // == 281474976710655
 		},
 
 
@@ -88,6 +88,120 @@ func TestUnixTimeMilli(t *testing.T) {
 
 
 		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(9998),
+			Expected:                              9998,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(9999),
+			Expected:                              9999,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(10000),
+			Expected:                              10000,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(10001),
+			Expected:                              10001,
+		},
+
+
+
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(99998),
+			Expected:                              99998,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(99999),
+			Expected:                              99999,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(100000),
+			Expected:                              100000,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(100001),
+			Expected:                              100001,
+		},
+
+
+
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(999998),
+			Expected:                              999998,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(999999),
+			Expected:                              999999,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(1000000),
+			Expected:                              1000000,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(1000001),
+			Expected:                              1000001,
+		},
+
+
+
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(9999998),
+			Expected:                              9999998,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(9999999),
+			Expected:                              9999999,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(10000000),
+			Expected:                              10000000,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(10000001),
+			Expected:                              10000001,
+		},
+
+
+
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(99999998),
+			Expected:                              99999998,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(99999999),
+			Expected:                              99999999,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(100000000),
+			Expected:                              100000000,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(100000001),
+			Expected:                              100000001,
+		},
+
+
+
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(999999998),
+			Expected:                              999999998,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(999999999),
+			Expected:                              999999999,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(1000000000),
+			Expected:                              1000000000,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(1000000001),
+			Expected:                              1000000001,
+		},
+
+
+
+		{
 			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(1778302347),
 			Expected:                              1778302347,
 		},
@@ -115,6 +229,17 @@ func TestUnixTimeMilli(t *testing.T) {
 		{
 			UUIDv7: uuidv7.GenerateAtUnixTime(222222222222),
 			Expected:                         222222222222000,
+		},
+
+
+
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(0xFFFFFFFF_FFFF),
+			Expected:                              0xFFFFFFFF_FFFF,
+		},
+		{
+			UUIDv7: uuidv7.GenerateAtUnixTimeMilli(281474976710655),
+			Expected:                              281474976710655,
 		},
 	}
 
